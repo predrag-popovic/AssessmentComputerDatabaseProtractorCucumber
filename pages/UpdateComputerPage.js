09423a07516ecd6e1de0f0edddd262283c7237ae
Predrag Popovic <42625068+predrag-popovic@users.noreply.github.com>
2019-05-09 16:37:48 +0200

    Update UpdateComputerPage.js

--- pages/UpdateComputerPage.js
+++ pages/UpdateComputerPage.js
@@ -2,6 +2,10 @@
 import { element, $ } from "protractor";
 import Globals from '../support/Globals';
 import base from '../elements/base';
+import button from '../elements/button_action';
+import input from '../elements/button_action';
+import scroll from '../elements/scroll_action';
+import wait from '../elements/wait_action';
 
 // Chai
 const globals = new Globals();
@@ -29,132 +33,141 @@ var filterByName = "searchsubmit";
 */
 var computerName = "GeckoComputerTest";
 
-class UpdateComputerPage extends base {
+class UpdateComputerPage {
+
+    constructor(){
+
+        this.base_action = new base();
+        this.button_action = new button();
+        this.input_action = new input();
+        this.wait_action = new wait();
+        this.scroll_action = new scroll();
+    }
 
     /***
     * Element for button add new Computer
     */
     get $buttonAddNewComputer() {
-        return this.findElementById(`${addNewComputer}`);
+        return this.base_action.findElementById(`${addNewComputer}`);
     }
 
     /***
     * Element for button create this computer
     */
     get $buttonCreateThisComputer() {
-        return this.findElementByCss(`${createThisComputer}`);
+        return this.base_action.findElementByCss(`${createThisComputer}`);
     }
 
     /***
     * Element for field Computer name
     */
     get $fieldComputerName() {
-        return this.findElementById(`${computerNameField}`);
+        return this.base_action.findElementById(`${computerNameField}`);
     }
 
     /***
     * Element for field Introduced Date
     */
     get $fieldIntroducedDate() {
-        return this.findElementById(`${introducedField}`);
+        return this.base_action.findElementById(`${introducedField}`);
     }
 
     /***
     * Element for field Discontinued Date
     */
     get $fieldDiscontinuedDate() {
-        return this.findElementById(`${discontinuedField}`);
+        return this.base_action.findElementById(`${discontinuedField}`);
     }
 
     /***
     * Element for search field
     */
     get $searchfield() {
-        return this.findElementById(`${searchField}`);
+        return this.base_action.findElementById(`${searchField}`);
     }
 
     /***
     * Element for search button
     */
     get $buttonSearch() {
-        return this.findElementById(`${filterByName}`);
+        return this.base_action.findElementById(`${filterByName}`);
     }
 
     /***
     * Element for Company field
     */
     get $openCompanyField() {
-        return this.findElementById(`${companyField}`);
+        return this.base_action.findElementById(`${companyField}`);
     }
 
     /***
     * List of computer
     */
     get $listOfComputer() {
-        return this.findElementByLinkText(`${computerName}` + `${timeStampInMs}`);
+        return this.base_action.findElementByLinkText(`${computerName}` + `${timeStampInMs}`);
     }
 
     /***
   * Actual result
   */
     get $actualResult() {
-        return this.findElementByCss(`${validationFieldNotification}`);
+        return this.base_action.findElementByCss(`${validationFieldNotification}`);
     }
 
     /***
     * Click on add New Computer
     */
     clickOnAddNewComputer() {
-        return this.waitAndClick(this.$buttonAddNewComputer, 10000);
+        return this.wait_action.waitAndClick(this.$buttonAddNewComputer, 10000);
     }
 
     /***
     * Click on button create this computer
     */
     clickOnButtonCreateThisComputer() {
-        return this.waitAndClick(this.$buttonCreateThisComputer, 10000);
+        return this.wait_action.waitAndClick(this.$buttonCreateThisComputer, 10000);
     }
 
     /***
     * Enter data in computer name field
     */
     enterComuterName(computerName) {
-        return this.waitAndType(this.$fieldComputerName, 10000, computerName + `${timeStampInMs}`);
+        return this.wait_action.waitAndType(this.$fieldComputerName, 10000, computerName + `${timeStampInMs}`);
     }
 
     /***
     * Enter data in Introduced Field
     */
     enterDateInIntroducedField(introducedData) {
-        return this.waitAndType(this.$fieldIntroducedDate, 10000, introducedData);
+        return this.wait_action.waitAndType(this.$fieldIntroducedDate, 10000, introducedData);
     }
 
     /***
     * Enter data in Discontinued Field
     */
     enterDateInDiscontinuedField(discontinuedDate) {
-        return this.waitAndType(this.$fieldDiscontinuedDate, 10000, discontinuedDate);
+        return this.wait_action.waitAndType(this.$fieldDiscontinuedDate, 10000, discontinuedDate);
     }
 
     /***
     * Enter data in Search Field
     */
     enterDataInSearchField(string) {
-        return this.waitAndType(this.$searchfield, 10000, string + `${timeStampInMs}`);
+        return this.wait_action.waitAndType(this.$searchfield, 10000, string + `${timeStampInMs}`);
     }
 
     /***
     * Click on button for Search
     */
     clickOnButtonSearch() {
-        return this.waitAndClick(this.$buttonSearch, 10000);
+        return this.wait_action.waitAndClick(this.$buttonSearch, 10000);
     }
 
     /***
     * Select one company from list
     */
     selectCompanyFromList() {
-        return this.waitAndClick(this.$openCompanyField, 10000)
+        return this.wait_action.waitAndClick(this.$openCompanyField, 10000)
             .then(() => this.$openCompanyField.$('[value="2"]').click());
     }
 
@@ -162,7 +175,7 @@ class UpdateComputerPage extends base {
     * Click on Computer name that is on table in Dashboard
     */
     clickOnComputerNameFromList() {
-        return this.waitAndClick(this.$listOfComputer);
+        return this.wait_action.waitAndClick(this.$listOfComputer);
     }
 
     /***
