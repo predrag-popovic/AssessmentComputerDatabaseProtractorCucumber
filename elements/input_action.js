7480e2155e393a8e15eea485e8beb70ff5b81f0b
Predrag Popovic <42625068+predrag-popovic@users.noreply.github.com>
2019-05-13 14:55:46 +0200

    Update input_action.js

--- elements/input_action.js
+++ elements/input_action.js
@@ -1,29 +1,140 @@
 'use strict';
+import base from '../elements/base';
+
 const protractor = require('protractor');
 const EC = protractor.ExpectedConditions;
 
 class input_action {
 
     constructor(){
-
+        
+        this.base_action = new base();
     }
 
     /**
-     * Type
+     * Find Element By Id and SendKeys
      * @param {protractor.ElementFinder} element - location of the element .
      * @param {string} text - set test data that we want to enter.
      */
-    clearAndType($element, text) {
-        return $element.sendKeys(text);
+    findElementByIdAndSendKeys($element, text) {
+        return this.base_action.findElementById($element).sendKeys(text);
     }
 
     /**
-     * Clear and type
+     * Find Element By Css and SendKeys
      * @param {protractor.ElementFinder} element - location of the element .
      * @param {string} text - set test data that we want to enter.
      */
-    clearAndType($element, text) {
-        return $element.clear().then(() => $element.sendKeys(text));
+    findElementByCssAndSendKeys($element, text) {
+        return this.base_action.findElementByCss($element).sendKeys(text);
+    }
+
+    /**
+     * Find Element By Xpath and SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByXpathAndSendKeys($element, text) {
+        return this.base_action.findElementByXpath($element).sendKeys(text);
+    }
+
+    /**
+     * Find Element By LinkText and SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByLinkTextAndSendKeys($element, text) {
+        return this.base_action.findElementByLinkText($element).sendKeys(text);
+    }
+
+    /**
+     * Find Element By Test Hook and SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByTestHookAndSendKeys($element, text) {
+        return this.base_action.findElementByTestHook($element).sendKeys(text);
+    }
+
+    /**
+     * Find Element By Name and SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByNameAndSendKeys($element, text) {
+        return this.base_action.findElementByName($element).sendKeys(text);
+    }
+
+    /**
+     * Find Element By Class Name and SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByClassNameAndSendKeys($element, text) {
+        return this.base_action.findElementByClassName($element).sendKeys(text);
+    }
+
+    /**
+     * Find Element By Id and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByIdAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementById($element).clear().then(() => this.base_action.findElementById($element).sendKeys(text));
+    }
+
+    /**
+     * Find Element By Css and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByCssAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementByCss($element).clear().then(() => this.base_action.findElementByCss($element).sendKeys(text));
+    }
+
+    /**
+     * Find Element By Xpath and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByXpathAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementByXpath($element).clear().then(() => this.base_action.findElementByXpath($element).sendKeys(text));
+    }
+
+    /**
+     * Find Element By Link Text and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByLinkTextAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementByLinkText($element).clear().then(() => this.base_action.findElementByLinkText($element).sendKeys(text));
+    }
+
+    /**
+     * Find Element By Test Hook and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByTestHookAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementByTestHook($element).clear().then(() => this.base_action.findElementByTestHook($element).sendKeys(text));
+    }
+
+    /**
+     * Find Element By Name and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByNameAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementByName($element).clear().then(() => this.base_action.findElementByName($element).sendKeys(text));
+    }
+
+    /**
+     * Find Element By Class Name and Clear before SendKeys
+     * @param {protractor.ElementFinder} element - location of the element .
+     * @param {string} text - set test data that we want to enter.
+     */
+    findElementByClassNameAndClearBeforeSendKeys($element, text) {
+        return this.base_action.findElementByClassName($element).clear().then(() => this.base_action.findElementByClassName($element).sendKeys(text));
     }
 
     /**
