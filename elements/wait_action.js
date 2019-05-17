104ebc9e5fa5d9db7811007ef1c7ab8e46700c98
Predrag Popovic <42625068+predrag-popovic@users.noreply.github.com>
2019-05-17 16:30:11 +0200

    Update wait_action.js

--- elements/wait_action.js
+++ elements/wait_action.js
@@ -2,6 +2,9 @@
 const protractor = require('protractor');
 const EC = protractor.ExpectedConditions;
 
+var log4js = require('log4js');
+var log = log4js.getLogger("base");
+
 class wait_action {
 
     constructor() {
@@ -13,6 +16,7 @@ class wait_action {
      * @param {protractor.ElementFinder} element - location of the element .
      */
     waitTillEnabled($element) {
+        log.info('Wait element till enabled:'+ $element);
         return browser.wait($element.isEnabled);
     }
 
@@ -22,6 +26,7 @@ class wait_action {
      * @param {string} timeOut - set time for timeout.
      */
     waitTillVisible($element, timeOut = 20000) {
+        log.info('Wait element till visible:'+ $element);
         return protractor.browser.wait(EC.visibilityOf($element), timeOut);
     }
 
@@ -31,6 +36,7 @@ class wait_action {
      * @param {string} timeOut - set time for timeout.
      */
     waitTillNotVisible($element, timeOut = 8000) {
+        log.info('Wait element till not visible:'+ $element);
         return protractor.browser.wait(EC.not(EC.visibilityOf($element)), timeOut);
     }
 
@@ -40,6 +46,7 @@ class wait_action {
      * @param {string} timeOut - set time for timeout.
      */
     waitTillGone($element, timeOut = 8000) {
+        log.info('Wait element till gone:'+ $element);
         return protractor.browser.wait(EC.stalenessOf($element), timeOut);
     }
 
@@ -49,6 +56,7 @@ class wait_action {
      * @param {string} timeOut - set time for timeout.
      */
     waitTillPresent($element, timeout = 20000) {
+        log.info('Wait element till present:'+ $element);
         return protractor.browser.wait(EC.presenceOf($element), timeout);
     }
 
@@ -57,6 +65,7 @@ class wait_action {
      * @param {protractor.ElementFinder} element - location of the element .
      */
     waitTillClickable($element) {
+        log.info('Wait element till clickable:'+ $element);
         return protractor.browser.wait(EC.visibilityOf($element), 40000).then(() => {
             return protractor.browser.wait(EC.elementToBeClickable($element), 8000);
         });
@@ -67,6 +76,7 @@ class wait_action {
      * @param {protractor.ElementFinder} element - location of the element .
      */
     waitUntilVisible($element) {
+        log.info('Wait element until visible:'+ $element);
         browser.wait(EC.presenceOf($element), 1000)
             .catch(err => err);
         return $element;
@@ -78,6 +88,7 @@ class wait_action {
      * @param {string} timeOut - set time for timeout.
      */
     waitAndClick($element, timeOut) {
+        log.info('Wait element and click'+ $element + 'waiting time:' + timeOut);
         return this.waitTillVisible($element, timeOut).then(() => $element.click());
     }
 
@@ -88,7 +99,7 @@ class wait_action {
      * @param {string} string - set test data that we want to enter.
      */
     waitAndType($element, timeOut, string) {
-
+        log.info('Wait element and type'+ $element + 'waiting time:' + timeOut + 'text:' + string);
         return this.waitTillVisible($element, timeOut).then(() => $element.sendKeys(string));
     }
 
