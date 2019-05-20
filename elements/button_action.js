07bcc35ca8d94b4199019c2f4c802aa5bdb2f806
Predrag Popovic <42625068+predrag-popovic@users.noreply.github.com>
2019-05-20 10:56:05 +0200

    Update button_action.js

--- elements/button_action.js
+++ elements/button_action.js
@@ -1,4 +1,6 @@
 'use strict';
+import base from '../elements/base';
+
 const protractor = require('protractor');
 const EC = protractor.ExpectedConditions;
 
@@ -8,7 +10,71 @@ var log = log4js.getLogger("base");
 class button_action {
 
     constructor() {
+        
+        this.base_action = new base();
+    }
+    
+    /**
+     * Click on Element By Id
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByIdAndClick($element) {
+        return this.base_action.findElementById($element).click()
+        .then(() => log.info('Find element by Id:'+'('+$element +')'+'and click'));
+    }
 
+    /**
+     * Click on Element By Css
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByCssAndClick($element) {
+        return this.base_action.findElementByCss($element).click()
+        .then(() => log.info('Find element by Css:'+'('+$element +')'+'and click'));
+    }
+
+    /**
+     * Click on Element By Xpath
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByXpathAndClick($element) {
+        return this.base_action.findElementByXpath($element).click()
+        .then(() => log.info('Find element by Xpath:'+'('+$element +')'+'and click'));
+    }
+
+    /**
+     * Click on Element By LinkText
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByLinkTextAndClick($element) {
+        return this.base_action.findElementByLinkText($element).click()
+        .then(() => log.info('Find element by LinkText:'+'('+$element +')'+'and click'));
+    }
+
+    /**
+     * Click on Element By TestHook
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByTestHookAndClick($element) {
+        return this.base_action.findElementByTestHook($element).click()
+        .then(() => log.info('Find element by TestHook:'+'('+$element +')'+'and click'));
+    }
+
+    /**
+     * Click on Element By Name
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByNameAndClick($element) {
+        return this.base_action.findElementByName($element).click()
+        .then(() => log.info('Find element by Name:'+'('+$element +')'+'and click'));
+    }
+
+    /**
+     * Click on Element By Class name
+     * @param {protractor.ElementFinder} element - location of the element .
+     */
+    findElementByClassNameAndClick($element) {
+        return this.base_action.findElementByClassName($element).click()
+        .then(() => log.info('Find element by Class name:'+'('+$element +')'+'and click'));
     }
 
     /**
